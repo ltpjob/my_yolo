a2e77d1a49bdaaf0c4dc46a04f03f09c56776fa8
ltpjob <ltpjob@163.com>
2017-12-07 13:12:33 +0800

    【mod】损失函数更新一部分。

--- train.py
+++ train.py
@@ -13,6 +13,27 @@ def DarknetConv2D_BN_Leaky(X,   filters, kernel_size, strides=(1, 1), padding='s
     return X
 
 
+def yolo_loss(y_true, y_pred):
+    y_pred = K.reshape(y_pred, (-1, cfg.CELL_SIZE, cfg.CELL_SIZE, cfg.BOXES_PER_CELL, 5 + cfg.CLASSES_COUNT))
+
+    pred_box_confidence = y_pred[:, :, :, :, 0:1]
+    pred_boxes = y_pred[:, :, :, :, 1:5]
+    pred_box_class_probs = y_pred[:, :, :, :, 5:]
+
+    true_box_confidence = y_true[:, :, :, :, 0:1]
+    true_boxes = y_true[:, :, :, :, 1:5]
+    true_box_class_probs = y_true[:, :, :, :, 5:]
+
+
+    print(box_confidence.shape)
+    print(boxes.shape)
+    print(box_class_probs.shape)
+
+
+
+
+
+
 def yolo_model_build(input_shape):
     X_input = Input(input_shape)
     #416to208
@@ -48,21 +69,16 @@ def yolo_model_build(input_shape):
     X = DarknetConv2D_BN_Leaky(X, 1024, (3, 3), strides=(1, 1))
     X = DarknetConv2D_BN_Leaky(X, 1024, (3, 3), strides=(1, 1))
     lab_filters = cfg.BOXES_PER_CELL*(5 + cfg.CLASSES_COUNT)
-    X = DarknetConv2D_BN_Leaky(X, lab_filters, (1, 1), strides=(1, 1))
-    predicts = K.reshape(X, (-1, cfg.CELL_SIZE, cfg.CELL_SIZE, cfg.BOXES_PER_CELL, 5 + cfg.CLASSES_COUNT))
 
-    box_confidence = predicts[:, :, :, :, 0:1]
-    boxes = predicts[:, :, :, :, 1:5]
-    box_class_probs = predicts[:, :, :, :, 5:]
+    X = Conv2D(lab_filters, (1, 1), strides=(1, 1), padding='same')(X)
+
 
 
 
     model = Model(inputs=X_input, outputs=X, name='my_yolo')
     model.summary()
 
-    print(box_confidence.shape)
-    print(boxes.shape)
-    print(box_class_probs.shape)
+
 
 
 yolo_model_build((cfg.IMAGE_SIZE, cfg.IMAGE_SIZE, 3))
